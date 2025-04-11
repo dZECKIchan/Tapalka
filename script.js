a25809dace4f6f176a06904807e2544c3a34664b
dZECKIchan <andrijcikut072@gmail.com>
2025-04-11 19:42:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ icon.addEventListener("click", () => {
   icon.addEventListener("animationend", () => {
     icon.classList.remove("animate-rotate");
   });
+
 const icon2 = document.getElementById("setingsico2");
 
 icon2.addEventListener("click", () => {
@@ -22,3 +23,29 @@ icon2.addEventListener("click", () => {
     window.location.href = "setings.html";
   }, 1200);
 });
+//MAIN FUNC
+
+let count = 0;
+let procent = 0;
+let progressbarmain = document.getElementById("progressbarmain");
+let progress = document.getElementById("progress");
+let textprogress = document.getElementById("textprogress");
+let monet = document.getElementById("monet");
+
+function addmonet(){
+    if(count >= 100){
+        monet.innerText = 'еееее, куда розогнався, я там щи не зробив'
+    }
+    else{
+        count++;
+        monet.innerText = count;
+        textprogress.innerText = `${count}/100`;
+        procent++;
+        progress.style.width = `${procent}%`;
+        moneyico.style.fill = `red`;
+    }
+}
+
+
+
+
